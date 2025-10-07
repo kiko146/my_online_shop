e00eab3fcba9cca80be84d8aba6c6ecb024af04f
Lenovo <victoryegon7426@gmail.com>
2025-10-07 19:43:15 +0300

    Fix port binding for Render

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, session, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import os
 # ---------------- FLASK APP SETUP ---------------- #
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
@@ -160,5 +160,8 @@ def logout():
     return redirect(url_for("home"))
 
 # ---------------- MAIN ---------------- #
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
